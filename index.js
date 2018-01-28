fe7b2e74b1f718dfe4bd791b5459e40f76302075
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-28 13:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase() === string
+  if (string.toLowerCase() === string){
   return("I can't hear you!")
-
-  string.toUpperCase() === string
+  } else if (string.toUpperCase() === string){
   return("YES INDEED!")
+  }
 }
 
 
