a570ad8baca4cc8c18a50b5e8376986679a8e163
Evangeline VIllena <vvanjie@yahoo.com>
2018-01-28 12:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = 'hello!'
-  lowercase.toLowerCase() === lowercase
+  var greet = 'hello!'
+  greet.toLowerCase() === lowercase
   return("I can't hear you!")
   
   var lowercase = 'hello!'
